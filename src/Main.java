2c7a73062c2e44f2619160a720acea19b310e68b
Martin Pedersen <42993919+MartinLieb@users.noreply.github.com>
2018-09-27 13:53:48 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner input= new Scanner();
+
+        double x1;
+        double x2;
+        double x3;
+        double x4;
+        double y1;
+        double y2;
+        double y3;
+        double y4;
+
+        double a;
+        double b;
+        double c;
+        double d;
+        double e;
+        double f;
+
+
+
+
+        System.out.println("Indtast 2 koordinater");
+
+        x1 = input.nextDouble();
+        y1 = input.nextDouble();
+        x2 = input.nextDouble();
+        y2 = input.nextDouble();
+
+        System.out.println("Indtast 2 koordinater");
+    }
+}
